95675dee58def452031fb04d143e4212ac9e8b73
AB <livelybokov@gmail.com>
2019-05-18 23:41:41 -0500

    cannonball

--- pirates.py
+++ pirates.py
@@ -1,5 +1,12 @@
 import random, pgzero, pgzrun
 
+class Projectile(Actor):
+    def __init__(self,img='cannonball',pos=(160,140),pointat=(170,130)
+                 ,damage=10):
+        Actor.__init__(self,img,pos)
+
+cannonball = Projectile()
+
 cannon = Actor('canon', (150, 150),anchor=('left', 'center'))
 
 def on_mouse_move(pos):
@@ -8,5 +15,6 @@ def on_mouse_move(pos):
 def draw():
     screen.clear()
     cannon.draw()
+    cannonball.draw()
 
 pgzrun.go()
