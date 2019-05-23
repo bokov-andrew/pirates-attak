84782e7086f9a89197324ed06152d049cfa2ccb3
A Bokov <lively.bokov@gmail.com>
2019-05-22 22:39:32 -0500

    create ball

--- pirates.py
+++ pirates.py
@@ -1,20 +1,31 @@
 import random, pgzero, pgzrun
 
+cannonball = None
+
 class Projectile(Actor):
     def __init__(self,img='cannonball',pos=(160,140),pointat=(170,130)
                  ,damage=10):
         Actor.__init__(self,img,pos)
+        
+class Cannon(Actor):
+    def __init__(self,img='canon',pos=(10,140)):
+        Actor.__init__(self,img,pos, anchor=('left', 'center'))
+    def fire(self, target):
+        cannonball = Projectile(pos = (self.right, self.top),pointat=target)
+        return(cannonball)
 
-cannonball = Projectile()
-
-cannon = Actor('canon', (150, 150),anchor=('left', 'center'))
+cannon = Cannon()
 
 def on_mouse_move(pos):
     cannon.angle = cannon.angle_to(pos)    
 
+def on_mouse_down(pos):
+    global cannonball
+    cannonball=cannon.fire(pos)
+
 def draw():
     screen.clear()
     cannon.draw()
-    cannonball.draw()
+    if cannonball: cannonball.draw()
 
 pgzrun.go()
