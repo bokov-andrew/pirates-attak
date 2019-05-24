239e49992223fe534a0dfad3bd22efa52b37cdcc
A Bokov <lively.bokov@gmail.com>
2019-05-23 22:14:35 -0500

    Fixed bug where cannonball was showing up in random places

--- pirates.py
+++ pirates.py
@@ -1,4 +1,5 @@
 import random, pgzero, pgzrun
+from pygame.math import Vector2
 
 cannonball = None
 
@@ -6,12 +7,22 @@ class Projectile(Actor):
     def __init__(self,img='cannonball',pos=(160,140),pointat=(170,130)
                  ,damage=10):
         Actor.__init__(self,img,pos)
+        print(self.pos)
         
 class Cannon(Actor):
     def __init__(self,img='canon',pos=(10,140)):
         Actor.__init__(self,img,pos, anchor=('left', 'center'))
+        self.length = self.width
+        self.pivot = self.midleft
     def fire(self, target):
-        cannonball = Projectile(pos = (self.right, self.top),pointat=target)
+        myv=Vector2()
+        myv.from_polar((self.length, self.angle))
+        myv.y = - myv.y
+        mypos = self.pivot + myv
+        print(self.pivot)
+        print(myv)
+        print(self.pivot + myv)
+        cannonball = Projectile(pos = mypos,pointat=target)
         return(cannonball)
 
 cannon = Cannon()
