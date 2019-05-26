721e073a2366aa33e94a2e5e525c599e3200d2ee
A Bokov <lively.bokov@gmail.com>
2019-05-25 22:50:30 -0500

    added gravity #1 #2

--- pirates.py
+++ pirates.py
@@ -1,13 +1,23 @@
 import random, pgzero, pgzrun
 from pygame.math import Vector2
 
+# global variables
 cannonball = None
-
+GRAVITY=1
 class Projectile(Actor):
     def __init__(self,img='cannonball',pos=(160,140),pointat=(170,130)
-                 ,damage=10):
+                 ,boost=0.1,damage=10):
         Actor.__init__(self,img,pos)
-        print(self.pos)
+        vel = Vector2()
+        vel.from_polar((self.distance_to(pointat)
+               ,self.angle_to(pointat)))
+        self.velocity = vel*boost
+        print ('velocity:',self.velocity)
+        
+    def move(self):
+        self.x += self.velocity.x
+        self.y -= self.velocity.y
+        self.velocity.y-=GRAVITY
         
 class Cannon(Actor):
     def __init__(self,img='canon',pos=(10,140)):
@@ -19,9 +29,6 @@ class Cannon(Actor):
         myv.from_polar((self.length, self.angle))
         myv.y = - myv.y
         mypos = self.pivot + myv
-        print(self.pivot)
-        print(myv)
-        print(self.pivot + myv)
         cannonball = Projectile(pos = mypos,pointat=target)
         return(cannonball)
 
@@ -34,9 +41,12 @@ def on_mouse_down(pos):
     global cannonball
     cannonball=cannon.fire(pos)
 
+def update():
+    if cannonball: cannonball.move()
+
 def draw():
     screen.clear()
-    cannon.draw()
     if cannonball: cannonball.draw()
-
+    cannon.draw()
+    
 pgzrun.go()
