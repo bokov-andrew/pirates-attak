caf227359a544f21e7d59bcac01f055d30be2328
A Bokov <lively.bokov@gmail.com>
2019-05-29 16:28:20 -0500

    Comment only edits, with notes on what we need in order to have bases/castles (and soon, ships)

--- pirates.py
+++ pirates.py
@@ -1,9 +1,16 @@
 import random, pgzero, pgzrun
 from pygame.math import Vector2
 
-# global variables
+####################
+# global variables #
+####################
 cannonball = None
 GRAVITY=1
+# Some more variables to add here (make up values for now):
+# p1points, gamePhase, gameTurn, WIDTH,HEIGHT, p1activeCannon
+
+
+
 class Projectile(Actor):
     def __init__(self,img='cannonball',pos=(160,140),pointat=(170,130)
                  ,boost=0.1,damage=10):
@@ -24,6 +31,7 @@ class Cannon(Actor):
         Actor.__init__(self,img,pos, anchor=('left', 'center'))
         self.length = self.width
         self.pivot = self.midleft
+        # add a self.state and set it to 0
     def fire(self, target):
         myv=Vector2()
         myv.from_polar((self.length, self.angle))
@@ -31,6 +39,12 @@ class Cannon(Actor):
         mypos = self.pivot + myv
         cannonball = Projectile(pos = mypos,pointat=target)
         return(cannonball)
+    
+    # define one more method called click, with arguments self and mousePos
+        # Return the value of self.collidepoint(mousePos)
+
+# define a debug function, no arguments
+    # In the body, just put: import pdb; pdb.set_trace()
 
 # Let's create a new class named Base. Like the ones above, make it inherit from
 # Actor like our other classes
@@ -49,15 +63,28 @@ class Cannon(Actor):
         # this adds the cannon
         # Soon, after we implement having points, we will also subtract the
         # the price of the cannon from the points the player has saved up
-        # 
+        
+    # define a takeDamage method with argument self, damage
+        # In the body, decrease self.hitpoints by damage
+        # If self.hitpoints < 0, print "Castle Destroyed"
 
+# We're going to get rid of the line below...
 cannon = Cannon()
+# ...and replace it with base = Base()
+# ...and then call the buyCannon() method of this newly created base object
+# Now you'll have a base with one cannon
 
 def on_mouse_move(pos):
+    # Check that base.cannons is not empty (anymore) and if it is not empty,
+    # Set the angle of base.cannons[0] to point at pos, like we're doing for
+    # the test cannon below. Get rid of the line below, since we no longer have
+    # a stand-alone test cannon, it's now part of a base
     cannon.angle = cannon.angle_to(pos)    
 
 def on_mouse_down(pos):
     global cannonball
+    # as above-- if base.cannons is not empty, fire base.cannons[0] instead of
+    # the test cannon
     cannonball=cannon.fire(pos)
 
 def update():
@@ -66,6 +93,10 @@ def update():
 def draw():
     screen.clear()
     if cannonball: cannonball.draw()
+    # check that base.cannons is not empty (anymore) and if it is not, use a for
+    # statement to FOO.draw() each item in base.cannons (base cannons is a list)
+    # instead of drawing just this one cannon (get rid of the line below)
     cannon.draw()
+    # Here also draw the base object
     
 pgzrun.go()
